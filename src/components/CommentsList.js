4c719c2c1c476e150114623673172e86bd673aff
soheee94 <hsh8616@naver.com>
2020-06-24 18:03:55 +0900

    불필요한 변수 제거

--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import CommentItem from "./CommentItem";
-import palette from "../palette";
 
 function CommentsList({ comments }) {
   return (
