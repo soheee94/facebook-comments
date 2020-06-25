df60bc9964ce6167c3dda02ce44a01992afc505e
한소희 <hsh8616@naver.com>
2020-06-25 23:37:05 +0900

    Updates

---
+++ precache-manifest.a401be2cb5e0813e2115693bbd67714b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07281e7e7919e6608957ed9addc40a77",
+    "url": "/facebook-comments/index.html"
+  },
+  {
+    "revision": "3e0f2f5f27d5d5d0805a",
+    "url": "/facebook-comments/static/css/main.758e65de.chunk.css"
+  },
+  {
+    "revision": "be13ecfeca1ebffcea4b",
+    "url": "/facebook-comments/static/js/2.1a6cff24.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/facebook-comments/static/js/2.1a6cff24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e0f2f5f27d5d5d0805a",
+    "url": "/facebook-comments/static/js/main.605ff202.chunk.js"
+  },
+  {
+    "revision": "dab1fa019e3389c5054d",
+    "url": "/facebook-comments/static/js/runtime-main.d38d8c11.js"
+  },
+  {
+    "revision": "ba9c9cc7f5616f99070a4dc08794a54e",
+    "url": "/facebook-comments/static/media/like.ba9c9cc7.png"
+  }
+]);
