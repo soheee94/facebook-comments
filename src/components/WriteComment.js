77ca425a044819f5c6006e2f42900bfe37d3794d
soheee94 <hsh8616@naver.com>
2020-06-25 14:20:57 +0900

    댓글 작성 기능 추가

--- src/components/WriteComment.js
+++ src/components/WriteComment.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import styled, { css } from "styled-components";
 import palette from "../palette";
+import { generateRandomID } from "../utils";
 
-function WriteComment() {
+function WriteComment({ onWrite }) {
+  const [user, setUser] = useState("");
+  const [comment, setComment] = useState("");
+
+  const onSubmit = e => {
+    // 새로고침 방지
+    e.preventDefault();
+    const data = {
+      id: generateRandomID(),
+      user: user,
+      text: comment,
+      created_at: Date.now(),
+      like_count: 0,
+      replies: []
+    };
+    onWrite(data);
+  };
+
   return (
-    <WriteCommentForm>
-      <Input type="text" name="user" placeholder="이름을 입력하세요" width="25%" />
-      <Input type="text" name="text" placeholder="댓글을 입력하세요" width="100%" />
+    <WriteCommentForm onSubmit={onSubmit}>
+      <Input type="text" name="user" value={user} onChange={e => setUser(e.target.value)} placeholder="이름을 입력하세요" width="25%" required />
+      <Input
+        type="text"
+        name="comment"
+        value={comment}
+        onChange={e => setComment(e.target.value)}
+        placeholder="댓글을 입력 후 엔터키를 누르세요"
+        width="100%"
+        required
+      />
+      <input type="submit" />
     </WriteCommentForm>
   );
 }
--- src/containers/WriteCommentContainer.js
+++ src/containers/WriteCommentContainer.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import WriteComment from "../components/WriteComment";
+import { writeComment } from "../modules/comments";
 
 function WriteCommentContainer() {
-  return <WriteComment />;
+  const dispatch = useDispatch();
+
+  const onWrite = data => {
+    dispatch(writeComment(data));
+  };
+
+  return <WriteComment onWrite={onWrite} />;
 }
 
 export default WriteCommentContainer;
